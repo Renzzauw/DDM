124cb02258774aac71f8782ea668ae43a8837cd9
CrookedGrandma <k.t.noot@live.nl>
2018-05-04 16:58:48 +0200

    hoi

--- Practical1/DDM_Practical1.py
+++ Practical1/DDM_Practical1.py
@@ -8,14 +8,18 @@
 # - Mail any bugs and inconsistencies you see to: uuinfoddm@gmail.com
 
 import bpy
+import os
 from mathutils import Vector as Vector
 from mathutils import Matrix as Matrix
+
+facecounter = 0
 
 # To view the printed output toggle the system console in "Window -> Toggle System Console"
 
 # This function is called when the DDM operator is selected in Blender.
 def DDM_Practical1(context):
-
+	
+	os.system('cls')
 	# TODO: get the triangles for the active mesh and use show_mesh() to display a copy
 	print("yeet")
 	show_mesh(get_triangles(context))
@@ -38,35 +42,38 @@ def DDM_Practical1(context):
 # Builds a mesh using a list of triangles
 def show_mesh(triangles):
 	# Create a mesh and object first
-	mesh = bpy.data.meshes.new("mesh")
-	obj = bpy.data.objects.new("newObject", mesh)
+	mesh = bpy.data.meshes.new("cube mesh")
+	obj = bpy.data.objects.new("new cube", mesh)
 	# Link the object to the scene
 	scene = bpy.context.scene
 	scene.objects.link(obj)
 	# Add the vertices of all triangles to a list
 	vertices = []
 	# Create a list for the faces too
-	i = 0
+	#i = 0
 	faces = []
 	for t in triangles:
 		# Create a face (list of vertices of the triangle) and increment the index of the vertices
 		face = []
-		face.append(i)
-		i += 1
-		face.append(i)
-		i += 1
-		face.append(i)
-		i += 1
+		# Add all the vertices and face indexes that don't exist yet
+		for x in range(0, len(t)):
+			# Check if vertex is already added, if yes add existing vertex to face, if no, create new vertex and add that
+			if t[x] in vertices:
+				face.append(vertices.index(t[x]))
+			else:
+				vertices.append(t[x])
+				addFace(face)
 		# Add the face to the list of faces
 		faces.append(face)
-		# Add the vertices to the list of vertices
-		vertices.append(t[0])       
-		vertices.append(t[1])
-		vertices.append(t[2])	
 	# add data to the mesh
 	mesh.from_pydata(vertices, [], faces)
 	# Update mesh changes
 	mesh.update()
+	
+def addFace(face):
+	global facecounter
+	face.append(facecounter)
+	facecounter += 1
 	
 # Returns the faces of the active object as a list of triplets of points
 def get_triangles(context):
@@ -85,7 +92,9 @@ def get_triangles(context):
 		faces.append(face)
 		
 	return faces
-	#return [(Vector([0, 0, 0]), Vector([1, 0, 0]), Vector([0, 1, 0])), (Vector([1, 0, 0]), Vector([1, 1, 0]), Vector([0, 1, 0]))]
+
+def testSquare():
+	return [(Vector([0, 0, 0]), Vector([1, 0, 0]), Vector([0, 1, 0])), (Vector([1, 0, 0]), Vector([1, 1, 0]), Vector([0, 1, 0]))]
 
 # Calculates the Euler characteristic of the given list of triangles
 def euler_characteristic(triangles):
